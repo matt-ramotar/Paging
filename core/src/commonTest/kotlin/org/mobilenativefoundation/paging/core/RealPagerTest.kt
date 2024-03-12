9328cb14be9b55ac382c1a9dbe2cef70dadae1ce
mramotar <mramotar@dropbox.com>
2024-03-12 10:43:05 -0400

    Refactor verifyPrefetching

--- core/src/commonTest/kotlin/org/mobilenativefoundation/paging/core/RealPagerTest.kt
+++ core/src/commonTest/kotlin/org/mobilenativefoundation/paging/core/RealPagerTest.kt
@@ -35,8 +35,6 @@ class RealPagerTest {
     private lateinit var timelineStoreFactory: TimelineStoreFactory
     private lateinit var timelineStore: MutableStore<PK, PD>
 
-    private val size = 10
-
     @BeforeTest
     fun setup() {
         backend = Backend()
@@ -70,8 +68,10 @@ class RealPagerTest {
         .build()
 
 
-    private suspend fun TurbineTestContext<PagingState<Id, K, P, D, E>>.verifyPrefetching() {
-
+    private suspend fun TurbineTestContext<PagingState<Id, K, P, D, E>>.verifyPrefetching(
+        pageSize: Int,
+        prefetchDistance: Int
+    ) {
         fun checkRange(data: List<SD>) {
             data.forEachIndexed { index, item ->
                 val id = index + 1
@@ -82,78 +82,61 @@ class RealPagerTest {
         val initial = awaitItem()
         assertIs<PagingState.Initial<Id, K, P, D, E>>(initial)
 
-        val loading = awaitItem()
-        assertIs<PagingState.Loading<Id, K, P, D, E>>(loading)
+        if (prefetchDistance > 0) {
+            val loading = awaitItem()
+            assertIs<PagingState.Loading<Id, K, P, D, E>>(loading)
 
-        // 1 - 10
-        val idle1 = awaitItem()
-        assertIs<PagingState.Data.Idle<Id, K, P, D, E>>(idle1)
-        checkRange(idle1.data)
-        assertEquals(size * 1, idle1.data.size)
+            val idle = awaitItem()
+            assertIs<PagingState.Data.Idle<Id, K, P, D, E>>(idle)
+            checkRange(idle.data)
+            assertEquals(pageSize, idle.data.size)
+        }
 
-        // 11 - 20
-        val loadingMore11 = awaitItem()
-        assertIs<PagingState.Data.LoadingMore<Id, K, P, D, E>>(loadingMore11)
+        var currentPage = 2
+        var expectedDataSize = pageSize
 
-        val idle11 = awaitItem()
-        assertIs<PagingState.Data.Idle<Id, K, P, D, E>>(idle11)
-        checkRange(idle11.data)
-        assertEquals(size * 2, idle11.data.size)
+        while (expectedDataSize < prefetchDistance) {
+            val loadingMore = awaitItem()
+            assertIs<PagingState.Data.LoadingMore<Id, K, P, D, E>>(loadingMore)
 
-        // 21 - 30
-        val loadingMore21 = awaitItem()
-        assertIs<PagingState.Data.LoadingMore<Id, K, P, D, E>>(loadingMore21)
+            val idle = awaitItem()
+            assertIs<PagingState.Data.Idle<Id, K, P, D, E>>(idle)
+            checkRange(idle.data)
+            expectedDataSize += pageSize
+            assertEquals(expectedDataSize, idle.data.size)
 
-        val idle21 = awaitItem()
-        assertIs<PagingState.Data.Idle<Id, K, P, D, E>>(idle21)
-        checkRange(idle21.data)
-        assertEquals(size * 3, idle21.data.size)
-
-        // 31 - 40
-        val loadingMore31 = awaitItem()
-        assertIs<PagingState.Data.LoadingMore<Id, K, P, D, E>>(loadingMore31)
-
-        val idle31 = awaitItem()
-        assertIs<PagingState.Data.Idle<Id, K, P, D, E>>(idle31)
-        checkRange(idle31.data)
-        assertEquals(size * 4, idle31.data.size)
-
-        // 41 - 50
-        val loadingMore41 = awaitItem()
-        assertIs<PagingState.Data.LoadingMore<Id, K, P, D, E>>(loadingMore41)
-
-        val idle41 = awaitItem()
-        assertIs<PagingState.Data.Idle<Id, K, P, D, E>>(idle41)
-        checkRange(idle41.data)
-        assertEquals(size * 5, idle41.data.size)
+            currentPage++
+        }
     }
 
     @Test
     fun testPrefetchingWhenPrefetchDistanceIsGreaterThan0() = testScope.runTest {
-        val initialKey: PK = PagingKey(0, TimelineKeyParams.Collection(size))
+        val pageSize = 10
+        val prefetchDistance = 50
+        val initialKey: PK = PagingKey(0, TimelineKeyParams.Collection(pageSize))
         val anchorPosition = MutableStateFlow(initialKey)
-        val pager = TestPager(initialKey, anchorPosition)
+        val pager = TestPager(initialKey, anchorPosition, pagingConfig = PagingConfig(pageSize, prefetchDistance, InsertionStrategy.APPEND))
 
         val state = pager.state
 
         state.test {
-            verifyPrefetching()
+            verifyPrefetching(pageSize, prefetchDistance)
             expectNoEvents()
         }
     }
 
     @Test
     fun testPrefetchingWhenPrefetchDistanceEquals0() = testScope.runTest {
-        val initialKey: PK = PagingKey(0, TimelineKeyParams.Collection(size))
+        val pageSize = 10
+        val prefetchDistance = 0
+        val initialKey: PK = PagingKey(0, TimelineKeyParams.Collection(pageSize))
         val anchorPosition = MutableStateFlow(initialKey)
-        val pager = TestPager(initialKey, anchorPosition, pagingConfig = PagingConfig(10, 0, InsertionStrategy.APPEND))
+        val pager = TestPager(initialKey, anchorPosition, pagingConfig = PagingConfig(pageSize, prefetchDistance, InsertionStrategy.APPEND))
 
         val state = pager.state
 
         state.test {
-            val initial = awaitItem()
-            assertIs<PagingState.Initial<Id, K, P, D, E>>(initial)
-
+            verifyPrefetching(pageSize, prefetchDistance)
             expectNoEvents()
         }
     }
