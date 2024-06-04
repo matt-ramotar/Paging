03d6caac6628c8e86981dd688660e5d47bc651ea
Matt Ramotar <matt.ramotar@uber.com>
2024-06-04 09:32:07 -0400

    TODO: Network operations

--- paging/src/commonMain/kotlin/org/mobilenativefoundation/storex/paging/internal/api/OperationManager.kt
+++ paging/src/commonMain/kotlin/org/mobilenativefoundation/storex/paging/internal/api/OperationManager.kt
@@ -4,6 +4,7 @@ import org.mobilenativefoundation.storex.paging.Identifiable
 import org.mobilenativefoundation.storex.paging.Quantifiable
 import org.mobilenativefoundation.storex.paging.custom.Operation
 
+// TODO(matt.ramotar@uber.com): Support local operations and network operations
 interface OperationManager<Id : Comparable<Id>, Q : Quantifiable<Id>, K : Any, V : Identifiable<Id, Q>> {
     fun addOperation(operation: Operation<Id, Q, K, V>)
     fun removeOperation(operation: Operation<Id, Q, K, V>)
